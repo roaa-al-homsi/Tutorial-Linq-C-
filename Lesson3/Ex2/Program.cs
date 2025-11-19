4b5ac1bb5a78e265bbc184a11c619f11c03eab5a
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-11-19 21:55:27 +0300

    ADD Folder Linq_Lesson_004_(Projection(select,selectMany,Zip))

--- Lesson3/Ex2/Program.cs
+++ Lesson3/Ex2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinqLesson3_Intro_for_Linq_;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,13 +10,33 @@ namespace Ex2
         static void Main(string[] args)
         {
             List<int> numbers = new List<int> { 1, 4, 6, 6 };
-            var evernNum = numbers.Where(x => x % 2 == 0);
-            foreach (int x in evernNum)
-            {
-                Console.WriteLine(x);
-            }
+            // var evenNum = numbers.Where(x => x % 2 == 0);
+            IEnumerable<int> evenNumUsingExtensionWhere = numbers.Where(x => x % 2 == 0);//Lazy Loading,,
+
+            //evenNum = evenNum.Where(x => x > 6);
+            //numbers.Add(10);
+            //numbers.Remove(4);
+            //foreach (int x in evenNum)//enumeration
+            //{
+            //    Console.WriteLine(x);
+            //}
             // var f = new ArrayList { 1, true, "HI" };
             // var mm=  f.where();
+
+
+            var EvenNumUsingWhereMehtod = Enumerable.Where(numbers, x => x % 2 == 0);
+            var EvenNumUsingQuerySyntax =
+                from n in numbers
+                where n % 2 == 0
+                select n;
+
+            evenNumUsingExtensionWhere.Print("evenNumUsingExtensionWhere");
+            EvenNumUsingWhereMehtod.Print("EvenNumUsingWhereMehtod");
+            EvenNumUsingQuerySyntax.Print("EvenNumUsingQuerySyntax");
+
+
+
+
 
 
             Console.ReadKey();
---
+++ Lesson3/Ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using LinqLesson3_Intro_for_Linq_;
+using System;
+using System.Linq;
+
+
+
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.LoadEmployees();
+
+            var empMale = employees.Where(e => e.Gender == "male");
+
+            empMale.Print("Male Employees");
+
+            var empSalaryOver300k = employees.Where(e => e.Salary > 300);
+            empSalaryOver300k.Print("Salary over 300k");
+
+            var maleInHRDepartment = empMale.Where(e => e.Department.ToLowerInvariant() == "hr");
+
+            maleInHRDepartment.Print("male in HR");
+
+
+            Console.ReadKey();
+        }
+    }
+}
