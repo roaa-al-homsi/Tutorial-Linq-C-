0b926586dcde006409efec2fa38cd07bde27fb62
roaa-al-homsi <roaakokalhomsi@gmail.com>
2026-02-06 21:15:21 +0300

    ADD lesson 17 (IEnumerable and IQueryable)

--- Linq_Lesson_008/LINQTut08/Program.cs
+++ Linq_Lesson_008/LINQTut08/Program.cs
@@ -1,16 +1,26 @@
 ﻿using LINQTut08.Shared;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LINQTut08
 {
     internal class Program
     {
+        class Student
+        {
+            public string Name { get; set; }
+            public string Grade { get; set; }
+            public int Score { get; set; }
+        }
         static void Main(string[] args)
         {
             RunGroupByExample();
             RunLookupExample();
             RunGroupByWithQuerySyntax();
+
+
+
             Console.ReadKey();
         }
 
@@ -66,6 +76,37 @@ namespace LINQTut08
             {
                 item.Print($"Employee in '{item.Key}' Department");
             }
+        }
+
+        private static void RunExampleForStudentsWithGroupBy()
+        {
+
+            List<Student> students = new List<Student>
+            {
+                new Student { Name = "Ali", Grade = "A", Score = 85 },
+                new Student { Name = "Mona", Grade = "B", Score = 90 },
+                new Student { Name = "Sara", Grade = "A", Score = 75 },
+                new Student { Name = "Ahmad", Grade = "C", Score = 60 },
+                new Student { Name = "Lina", Grade = "B", Score = 80 },
+                new Student { Name = "Zara", Grade = "A", Score = 95 }
+            };
+
+
+            var result = students.GroupBy(s => s.Grade).Select(g => new { Count = g.Count(), Grade = g.Key, Min = g.Min(s => s.Score), Max = g.Max(s => s.Score), Students = g, Average = g.Average(s => s.Score) }).OrderBy(g => g.Grade);
+
+            foreach (var group in result)
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine($"Grade:{group.Grade} | Count: {group.Count} | Min: {group.Min} | Max : {group.Max} |Average : {group.Average}");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Students:");
+                foreach (var item in group.Students)
+                {
+
+                    Console.WriteLine(item.Name);
+                }
+            }
+
         }
     }
 }
